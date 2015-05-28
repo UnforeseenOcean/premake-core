e86d871b3dc49b211d68452b67f4ee731018a00c
Tom van Dijck <tvandijck@tvandijck.corp.blizzard.net>
2015-05-28 12:40:23 -0700

    [core] -Og is unrecognized.

--- tests/tools/test_gcc.lua
+++ tests/tools/test_gcc.lua
@@ -191,7 +191,7 @@
 	function suite.cflags_onOptimizeDebug()
 		optimize "Debug"
 		prepare()
-		test.contains({ "-Og" }, gcc.getcflags(cfg))
+		test.contains({ "-O" }, gcc.getcflags(cfg))
 	end
 
 
